ce77f004d78fbdc007541121e6b7f8f4ee130f34
Row-Bell <robel.arega2@gmail.com>
2021-01-01 23:12:01 -0600

    graph 3d -Lines

--- graph.py
+++ graph.py
@@ -296,7 +296,7 @@ plt2.legend()
 plt3.legend()
 
 # Function to show plot
-plt.show()'''
+plt.show()
 
 ##------------3D plotting-------------######
 from mpl_toolkits.mplot3d import axes3d
@@ -328,4 +328,36 @@ ax1.set_ylabel('y-axis')
 ax1.set_zlabel('z-axis')
 
 #function to show the plot
+plt.show()'''
+
+##------------3D plotting: Lines-------------######
+#importing required modules
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+# setting a custom style to use
+style.use('ggplot')
+
+# create a new figure for plotting
+fig = plt.figure()
+
+#create a new subplot on our figure
+ax1 = fig.add_subplot(111,projection='3d')
+
+#defining x, y, z co-ordinates
+x = np.random.randint(0, 10, size = 5)
+y = np.random.randint(0, 10, size = 5)
+z = np.random.randint(0, 10, size = 5)
+
+#plotting the points on subplot
+ax1.plot_wireframe(x,y,z)
+
+#setting the labels
+ax1.set_xlabel('x-axis')
+ax1.set_ylabel('y-axis')
+ax1.set_zlabel('z-axis')
+
+#Function plotting the graph
 plt.show()
